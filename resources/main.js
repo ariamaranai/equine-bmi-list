0d86c41f61f9b77df6d6d1a49720bbdd4f5141bb
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2024-01-17 23:03:28 +0900

    Add files via upload

---
+++ resources/main.js
@@ -0,0 +1,5 @@
+oninput =a=> {
+  let e = document.body.children, r = 5, t = 0;
+  while (e[r].className = e[r].textContent.includes(a.target.value) ? '' : (++t,'i'), ++r < $total);
+  i.textContent = t
+}
