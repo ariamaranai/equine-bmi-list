dac5f97bc4158d48315088ad15de0dda63eff3ec
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2024-04-21 21:56:47 +0900

    Add files via upload

--- resources/build.js
+++ resources/build.js
@@ -1,43 +1,39 @@
 import heights from "./height.json";
 
-let total = heights.length;
-let css = (await Bun.file("main.css").text()).replaceAll(/\n| {2}|\s(?={)|(?<=\:)\s/g, "");
-let js = (await Bun.file("main.js").text()).replaceAll(/\s/g, "").replaceAll("let", "let ").replaceAll("$total", total);
-let html = (await Bun.file("main.htm").text()).replaceAll(/\n/g, "").replaceAll("$total", total).replace("/*css*/", css).replace("/*js*/", js);
-let hrefing =(href, name)=> `<a href=${href.slice(href[12] == "j" ? -11 : 6)}>${name}</a>`
-
-let heightHTML = html + "HEIGHT  / <a href=//ariamaranai.github.io/equine-height-list/weight/ target=_self>WEIGHT</a>  / <a href=//ariamaranai.github.io/equine-height-list/bmi/ target=_self>BMI</a>  / <a href=//ariamaranai.github.io/>@ariamaranai</a>";
-for (let i = 0; i < heights.length; ++i) {
-  let item = heights[i];
-  let hh = item.hh;
-  let wt  = item.wt;
-  let bmi = item.bmi == -1 ? (item.bmi = wt ? ((wt / ((hh / 100) ** 2)) + .05) : 0) : item.bmi;
-  heightHTML += `<p>${hrefing(item.href, item.name)}${bmi ? `${hh}  ${item.trust ? wt : `<i>${wt}</i>`}  ${String(bmi).padEnd(5, ".0")}` : `${hh}   -     -  `}  ${item.year}.${item.cnty.padEnd(4)}${hrefing(item.shref, item.sire)} /${hrefing(item.dshref, item.dsire)}`;
-}
-Bun.write("../index.htm", heightHTML);
-console.log(`size: ${Bun.gzipSync(Buffer.from(heightHTML)).length}`);
-
-let weightHTML = html + "<a href=//ariamaranai.github.io/equine-height-list/ target=_self>HEIGHT</a>  /  WEIGHT  / <a href=//ariamaranai.github.io/equine-height-list/bmi/ target=_self>BMI</a>  / <a href=//aiamraranai.github.io/>@ariamaranai</a>";
-let weights = heights.toSorted((a, b) => a.wt > b.wt ? 1 : a.wt < b.wt ? -1 : 0);
-for (let i = 0; i < weights.length; ++i) {
-  let item = weights[i];
-  let hh = item.hh;
-  let wt  = item.wt;
-  let bmi = item.bmi;
-  weightHTML += `<p>${hrefing(item.href, item.name)}${bmi ? `${hh}  ${item.trust ? wt : `<i>${wt}</i>`}  ${String(bmi).padEnd(5, ".0")}` : `${hh}   -     -  `}  ${item.year}.${item.cnty.padEnd(4)}${hrefing(item.shref, item.sire)} /${hrefing(item.dshref, item.dsire)}`;
-}
-Bun.write("../weight/index.htm", weightHTML);
-
-let bmiHTML = html + "<a href=//ariamaranai.github.io/equine-height-list/ target=_self>HEIGHT</a>  / <a href=//ariamaranai.github.io/equine-height-list/weight/ targert=_self>WEIGHT</a>  /  BMI  / <a href=//ariamaranai.github.io/>@ariamaranai</a>";
+let wts = heights.toSorted((a, b) => a.wt > b.wt ? 1 : a.wt < b.wt ? -1 : 0);
 let bmis = heights.toSorted((a, b) => a.bmi > b.bmi ? 1 : a.bmi < b.bmi ? -1 : 0);
-for (let i = 0; i < bmis.length; ++i) {
-  let item = bmis[i];
-  let hh = item.hh;
-  let wt  = item.wt;
-  let bmi = item.bmi;
-  bmiHTML += `<p>${hrefing(item.href, item.name)}${bmi ? `${hh}  ${item.trust ? wt : `<i>${wt}</i>`}  ${String(bmi).padEnd(5, ".0")}` : `${hh}   -     -  `}  ${item.year}.${item.cnty.padEnd(4)}${hrefing(item.shref, item.sire)} /${hrefing(item.dshref, item.dsire)}`;
-}
-Bun.write("../bmi/index.htm", bmiHTML);
+let total = heights.length;
+let js = (await Bun.file("main.js").text()).replaceAll(/(?<!let|new)\s/g, "").replace("async", "async ").replace("await", "await ").replaceAll("$total", total);
+let hhCSS = (await Bun.file("main.css").text()).replaceAll(/\n| {2}|\s(?={)|(?<=\:)\s/g, "");
+let hhHTML = (await Bun.file("main.htm").text()).replaceAll(/\n/g, "").replaceAll("$total", total).replace("/*css*/", hhCSS);
+let wtHTML = hhHTML.replace("<option>Weight", "<option selected>Weight");
+let bmiHTML = hhHTML.replace("<option>Bmi", "<option selected>Bmi");
+let hrefing =href=> href[12] == "j" ? href.slice(-11) : "//www.pedigreequery.com/" + href.slice(30).split("+").map(v => v[0].toUpperCase() + v.slice(1)).join("+");
+let footer = `</p>by <a href=//ariamaranai.github.io/ style=font-family:unset>@ariamaranai<script>${js}</script>`;
 
-let json = JSON.stringify(heights, null, 2).replaceAll('"bmi": "', '"bmi": ').replaceAll(`",\u000a    "trust"`, `,\u000a    "trust"`);
-Bun.write("./height.json", json);
+for (let i = 0; i < heights.length; ++i) {
+  {
+    let item = heights[i];
+    let { name: name, href: href, sname: sname, gname: gname, hh: hh, wt: wt, bmi: bmi } = item;
+    hhHTML += `<p><a href=${hrefing(href)}>${name}</a>${hh} ${wt} ${bmi.toFixed(1)}<s>${sname}・${gname}</s>`;
+  }
+  {
+    let item = wts[i];
+    let { name: name, href: href, sname: sname, gname: gname, hh: hh, wt: wt, bmi: bmi } = item;
+    wtHTML += `<p><a href=${hrefing(href)}>${name}</a>${hh} ${wt} ${bmi.toFixed(1)}<s>${sname}・${gname}</s>`;
+  }
+  {
+    let item = bmis[i];
+    let { name: name, href: href, sname: sname, gname: gname, hh: hh, wt: wt, bmi: bmi } = item;
+    bmiHTML += `<p><a href=${hrefing(href)}>${name}</a>${hh} ${wt} ${bmi.toFixed(1)}<s>${sname}・${gname}</s>`;
+  }
+}
+
+hhHTML += footer;
+wtHTML += footer;
+bmiHTML += footer;
+
+Bun.write("../hh.htm", hhHTML);
+Bun.write("../wt.htm", wtHTML);
+Bun.write("../bmi.htm", bmiHTML);
+console.log(`size: ${Bun.gzipSync(Buffer.from(hhHTML)).length}`);
--- resources/main.js
+++ resources/main.js
@@ -1,5 +1,8 @@
-oninput =a=> {
-  let e = document.body.children, r = 5, t = 0;
-  while (e[r].className = e[r].textContent.includes(a.target.value) ? '' : (++t,'i'), ++r < $total);
-  i.textContent = t
-}
+addEventListener('wheel', n => n.preventDefault(document.body.scrollTop += n.deltaY > 0 ? Number.isInteger(devicePixelRatio) && 32 - document.body.scrollTop % 32 || 32 : Number.isInteger(devicePixelRatio) && -document.body.scrollTop % 32 || -32), { passive: !1 }),
+ondragstart =n=> !1,
+a.oninput =n=> {
+  let e = document.body.children, r = $total, t = 0;
+  while (e[r].className = e[r].textContent.includes(n.target.value) ? (++t,'') : 'a', --r);
+  p.textContent = t
+},
+r.onchange =n=> open("//ariamaranai.github.io/equine-height-list/" + ["","weight/","bmi/"][r.selectedIndex])
