9a829ac98f803b6e12c44c8829a1b562c4a76ce0
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2024-05-01 04:46:10 +0900

    Add files via upload

--- resources/main.js
+++ resources/main.js
@@ -1,8 +1,8 @@
-addEventListener('wheel', n => n.preventDefault(document.body.scrollTop += n.deltaY > 0 ? Number.isInteger(devicePixelRatio) && 24 - document.body.scrollTop % 24 || 24 : Number.isInteger(devicePixelRatio) && -document.body.scrollTop % 24 || -24), { passive: !1 }),
+addEventListener('wheel', n => n.preventDefault(document.body.scrollTop += n.deltaY > 0 ? Number.isInteger(devicePixelRatio) && 32 - document.body.scrollTop % 32 || 32 : Number.isInteger(devicePixelRatio) && -document.body.scrollTop % 32 || -32), { passive: !1 }),
 ondragstart =n=> !1,
 a.oninput =n=> {
   let e = document.body.children, r = $total, t = 0;
-  while (e[r].className = e[r].textContent.includes(n.target.value) ? (++t,'') : 's', --r);
+  while (e[r].className = e[r].textContent.includes(n.target.value) ? (++t,'') : 'p', --r);
   u.textContent = t
 },
 i.onchange =n=> (location.href = '//ariamaranai.github.io/equine-bmi-list/' + ['','wt.htm','bmi.htm'][i.selectedIndex])
