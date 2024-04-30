2e04a86f86388af81c9a2169a7b5a083ea9a2769
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2024-05-01 04:36:04 +0900

    Add files via upload

--- resources/build.js
+++ resources/build.js
@@ -15,17 +15,17 @@ for (let i = 0; i < heights.length; ++i) {
   {
     let item = heights[i];
     let { name: name, href: href, sname: sname, gname: gname, hh: hh, wt: wt, bmi: bmi } = item;
-    hhHTML += `<p><a href=${hrefing(href)}>${name}</a><s>${hh} ${wt} ${bmi.toFixed(1)} </s>${sname}・${gname}`;
-  }
+    hhHTML += `<p><a href=${hrefing(href)}>${name}</a>${hh} ${wt}<u>(${bmi.toFixed(1)})</u><i>${sname}\u000a${gname}</i>`;
+  }  
   {
     let item = wts[i];
     let { name: name, href: href, sname: sname, gname: gname, hh: hh, wt: wt, bmi: bmi } = item;
-    wtHTML += `<p><a href=${hrefing(href)}>${name}</a><s>${hh} ${wt} ${bmi.toFixed(1)} </s>${sname}・${gname}`;
+    wtHTML += `<p><a href=${hrefing(href)}>${name}</a>${hh} ${wt}<u>(${bmi.toFixed(1)})</u><i>${sname}\u000a${gname}</i>`;
   }
   {
     let item = bmis[i];
     let { name: name, href: href, sname: sname, gname: gname, hh: hh, wt: wt, bmi: bmi } = item;
-    bmiHTML += `<p><a href=${hrefing(href)}>${name}</a><s>${hh} ${wt} ${bmi.toFixed(1)} </s>${sname}・${gname}`;
+    bmiHTML += `<p><a href=${hrefing(href)}>${name}</a>${hh} ${wt}<u>(${bmi.toFixed(1)})</u><i>${sname}\u000a${gname}</i>`;
   }
 }
 
--- resources/main.js
+++ resources/main.js
@@ -1,8 +1,8 @@
-addEventListener('wheel', n => n.preventDefault(document.body.scrollTop += n.deltaY > 0 ? Number.isInteger(devicePixelRatio) && 32 - document.body.scrollTop % 32 || 32 : Number.isInteger(devicePixelRatio) && -document.body.scrollTop % 32 || -32), { passive: !1 }),
+addEventListener('wheel', n => n.preventDefault(document.body.scrollTop += n.deltaY > 0 ? Number.isInteger(devicePixelRatio) && 24 - document.body.scrollTop % 24 || 24 : Number.isInteger(devicePixelRatio) && -document.body.scrollTop % 24 || -24), { passive: !1 }),
 ondragstart =n=> !1,
 a.oninput =n=> {
   let e = document.body.children, r = $total, t = 0;
   while (e[r].className = e[r].textContent.includes(n.target.value) ? (++t,'') : 's', --r);
-  s.textContent = t
+  u.textContent = t
 },
-b.onchange =n=> open('//ariamaranai.github.io/equine-bmi-list/' + ['','wt.htm','bmi.htm'][b.selectedIndex])
+i.onchange =n=> (location.href = '//ariamaranai.github.io/equine-bmi-list/' + ['','wt.htm','bmi.htm'][i.selectedIndex])
