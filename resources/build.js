e8c17718f071f6de2ea809ad2bb6921a9bc2705c
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2024-05-01 09:17:11 +0900

    Add files via upload

--- resources/build.js
+++ resources/build.js
@@ -9,23 +9,23 @@ let hhHTML = (await Bun.file("main.htm").text()).replaceAll(/\n/g, "").replaceAl
 let wtHTML = hhHTML.replace("<option>Weight", "<option selected>Weight");
 let bmiHTML = hhHTML.replace("<option>BMI", "<option selected>BMI");
 let hrefing =href=> href[12] == "j" ? href.slice(-11) : "//www.pedigreequery.com/" + href.slice(30).split("+").map(v => v[0].toUpperCase() + v.slice(1)).join("+");
-let footer = `</p>by <a href=//ariamaranai.github.io/ style=display:contents>@ariamaranai</a><script>${js}</script>`;
+let footer = `</p><a href=//ariamaranai.github.io/ style=display:contents>@ariamaranai</a><script>${js}</script>`;
 
 for (let i = 0; i < heights.length; ++i) {
   {
     let item = heights[i];
     let { name: name, href: href, sname: sname, gname: gname, hh: hh, wt: wt, bmi: bmi } = item;
-    hhHTML += `<p><a href=${hrefing(href)}>${name}</a>${hh} ${wt}<u>(${bmi.toFixed(1)})</u><i>${sname}\u000a${gname}</i>`;
+    hhHTML += `<p><a href=${hrefing(href)}>${name}</a>${hh} ${wt}(${bmi.toFixed(1)})<i>${sname}\u000a${gname}</i>`;
   }  
   {
     let item = wts[i];
     let { name: name, href: href, sname: sname, gname: gname, hh: hh, wt: wt, bmi: bmi } = item;
-    wtHTML += `<p><a href=${hrefing(href)}>${name}</a>${hh} ${wt}<u>(${bmi.toFixed(1)})</u><i>${sname}\u000a${gname}</i>`;
+    wtHTML += `<p><a href=${hrefing(href)}>${name}</a>${hh} ${wt}(${bmi.toFixed(1)})<i>${sname}\u000a${gname}</i>`;
   }
   {
     let item = bmis[i];
     let { name: name, href: href, sname: sname, gname: gname, hh: hh, wt: wt, bmi: bmi } = item;
-    bmiHTML += `<p><a href=${hrefing(href)}>${name}</a>${hh} ${wt}<u>(${bmi.toFixed(1)})</u><i>${sname}\u000a${gname}</i>`;
+    bmiHTML += `<p><a href=${hrefing(href)}>${name}</a>${hh} ${wt}(${bmi.toFixed(1)})<i>${sname}\u000a${gname}</i>`;
   }
 }
 
