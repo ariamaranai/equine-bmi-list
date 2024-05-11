7e9e3a8cff2b4f5901f2c652520bb36b52df7337
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2024-05-11 15:58:47 +0900

    Add files via upload

--- resources/build.js
+++ resources/build.js
@@ -1,45 +1,36 @@
-import horses from "./mstn.json";
+import heights from "./height.json";
 
-let total = horses.length;
-let css = (await Bun.file("main.css").text()).replaceAll(/\n| {2}|\s(?={)|(?<=\:)\s/g, "");
-let js = (await Bun.file("main.js").text()).replaceAll(/\s/g, "").replaceAll("let", "let ").replaceAll("$total", total);
-let html = (await Bun.file("main.htm").text()).replaceAll(/\n/g, "").replaceAll("$total", total).replace("/*css*/", css).replace("/*js*/", js);
-let mainNameForDebug = [];
-let hrefsForDebug = [];
-let namesForDebug = [];
+let wts = heights.toSorted((a, b) => a.wt > b.wt ? 1 : a.wt < b.wt ? -1 : 0);
+let bmis = heights.toSorted((a, b) => a.bmi > b.bmi ? 1 : a.bmi < b.bmi ? -1 : 0);
+let total = heights.length;
+let js = (await Bun.file("main.js").text()).replaceAll(/(?<!let|new)\s/g, "").replace("async", "async ").replace("await", "await ").replaceAll("$total", total);
+let hhCSS = (await Bun.file("main.css").text()).replaceAll(/\n| {2}|\s(?={)|(?<=\:)\s/g, "");
+let hhHTML = (await Bun.file("main.htm").text()).replaceAll(/\n/g, "").replaceAll("$total", total).replace("/*css*/", hhCSS);
+let wtHTML = hhHTML.replace("<option>Weight", "<option selected>Weight").replace("- Height", "- Weight");
+let bmiHTML = hhHTML.replace("<option>BMI", "<option selected>BMI").replace("- Height", "- BMI");
+let hrefing =href=> href[12] == "j" ? href.slice(-11) : "//www.pedigreequery.com/" + href.slice(30).split("+").map(v => v[0].toUpperCase() + v.slice(1)).join("+");
+let footer = `</p><a href=//ariamaranai.github.io/>@ariamaranai</a><script>${js}</script>`;
 
-for (let i = 0; i < total; ++i) {
-  html += "<p>";
-  // Debug
-  let mainName = horses[i][0].name;
-  mainName != "-" && mainNameForDebug.includes(mainName) ? console.log(mainName) : mainNameForDebug.push(mainName);
-
-  for (let j = 0; j < 4; ++j) {
-    let { name, year, type, href, mstn } = horses[i][j];
-    if (href) {
-      // Debug
-      let hrefsIdx = hrefsForDebug.indexOf(href);
-      if (hrefsIdx < 0) {
-        hrefsForDebug.push(href);
-        namesForDebug.push(name);
-      } else if (namesForDebug[hrefsIdx] != name) {
-        console.log(horses[i]);
-      }
-
-      let host = href.slice(8, href.indexOf("/", 10));
-      if (host == "www.allbreedpedigree.com")
-        href = " href=" + href.slice(33).split("+").map(v => v.length < 3 ? v : v[0].toUpperCase() + v.slice(1)).join("+");
-      else if (host == "www.pedigreequery.com")
-        href = " href=//www.pedigreequery.com/" + href.slice(30).split("+").map(v => v.length < 3 ?  v : v[0].toUpperCase() + v.slice(1)).join("+");
-      else
-        href = " href=" + href.slice(6);
-    }
-    mstn = mstn ? mstn == "CC" ? " s" : mstn == "CT" ? " a" : " e" : "";
-    html += `<a${href}${mstn}>${name}${year ? "\u000a" + year : ""}${year ? " " + type : type ? "\u000a" + type : ""}`;
+for (let i = 0; i < heights.length; ++i) {
+  {
+    let { name, href, sname, gname, hh, wt, bmi } = heights[i];
+    hhHTML += `<p><a href=${hrefing(href)}>${name}</a>${hh} ${wt}(${bmi.toFixed(1)})<i>${sname}\u000a${gname}</i>`;
   }
-  html += "</a>"
+  {
+    let { name, href, sname, gname, hh, wt, bmi } = wts[i];
+    wtHTML += `<p><a href=${hrefing(href)}>${name}</a>${hh} ${wt}(${bmi.toFixed(1)})<i>${sname}\u000a${gname}</i>`;
+  }
+  {
+    let { name, href, sname, gname, hh, wt, bmi } = bmis[i];
+    bmiHTML += `<p><a href=${hrefing(href)}>${name}</a>${hh} ${wt}(${bmi.toFixed(1)})<i>${sname}\u000a${gname}</i>`;
+  }
 }
 
-html += "</p>by <a href=//ariamaranai.github.io/ Class=p>@ariamaranai";
-Bun.write("../index.htm", html);
-console.log(`size: ${Bun.gzipSync(Buffer.from(html)).length}`);
+hhHTML += footer;
+wtHTML += footer;
+bmiHTML += footer;
+
+Bun.write("../index.htm", hhHTML);
+Bun.write("../weight/index.htm", wtHTML);
+Bun.write("../bmi/index.htm", bmiHTML);
+console.log(`size: ${Bun.gzipSync(Buffer.from(hhHTML)).length}`);
