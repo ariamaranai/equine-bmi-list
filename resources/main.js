3c2e4e61ab77b3a921f6e6559e770cb2268bcbc4
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2024-10-08 08:39:06 +0900

    Add files via upload

--- resources/main.js
+++ resources/main.js
@@ -4,18 +4,17 @@ addEventListener("wheel", Number.isInteger(devicePixelRatio) ?
   e => e.preventDefault(t.scrollTop += e.deltaY > 0 ? 36 - t.scrollTop % 36 || 36 : - t.scrollTop % 36 || -36) :
   e => e.preventDefault(t.scrollTop += e.deltaY > 0 ? 36 : -36), { passive: !1 }),
 oninput =e=> {
-  if (a ??= [...t.children], r = $total, e = e.target.selectedIndex) {
-    if (--e < 3)
-      while (t.appendChild(a[$orders[e][r]]), --r);
+  if (a ??= [...t.children], r = $total, l = (e = e.target).selectedIndex) {
+    if (--l < 3)
+      while (t.appendChild(a[$orders[l][r]]), --r);
     else 
       (location.href = "//ariamaranai.github.io")
   } else {
     d ??= a.map(e => e.textContent),
-    e = e.target.value;
-    let t = 0;
-    while (a[r].setAttribute("style", d[r].includes(e) ? (++t, "") : "display:none"), --r);
-    t.textContent = t
+    e = e.value, l = 0;
+    while (a[r].setAttribute("style", d[r].includes(e) ? (++l, "") : "display:none"), --r);
+    s.textContent = l
   }
 };
-let t = document.body, a, d, r
+let t = document.body, l, a, d, r
 }
