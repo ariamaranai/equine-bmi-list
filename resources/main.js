1d276b094462b472329bd59024e19a5b25c6f48a
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2024-11-04 11:11:36 +0900

    Add files via upload

--- resources/main.js
+++ resources/main.js
@@ -1,23 +1,22 @@
 ondragstart =e=> !1;
 {
-addEventListener("wheel", Number.isInteger(devicePixelRatio) ?
-  e => e.preventDefault(t.scrollTop += e.deltaY > 0 ? 36 - t.scrollTop % 36 || 36 : - t.scrollTop % 36 || -36) :
-  e => e.preventDefault(t.scrollTop += e.deltaY > 0 ? 36 : -36), { passive: !1 }),
 oninput =e=> {
   if (a ??= [...t.children], r = $total, l = (e = e.target).selectedIndex) {
     if (--l < 3)
       while (t.appendChild(a[$orders[l][r]]), --r);
     else
       (location.href = "//ariamaranai.github.io")
-  } else if (l == 0) {
-    alert(1);
-    while (t.prepend(a[r]), --r);
-  } else {
+  } else if (l == 0)
+    while (t.prepend(a[--r]), r);
+  else {
     d ??= a.map(e => e.textContent),
     e = e.value, l = 0;
     while (a[r].setAttribute("style", d[r].includes(e) ? (++l, "") : "display:none"), --r);
     s.textContent = l
   }
-};
+},
+addEventListener("wheel", Number.isInteger(devicePixelRatio) ?
+  e => e.preventDefault(t.scrollTop += e.deltaY > 0 ? 36 - t.scrollTop % 36 || 36 : - t.scrollTop % 36 || -36) :
+  e => e.preventDefault(t.scrollTop += e.deltaY > 0 ? 36 : -36), { passive: !1 });
 let t = document.body, l, a, d, r
 }
