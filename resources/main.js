5051d8f8199cd1cc55034aec47bb2bb5b5133544
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2024-12-07 12:35:12 +0900

    Add files via upload

--- resources/main.js
+++ resources/main.js
@@ -3,7 +3,7 @@ ondragstart =e=> !1;
 oninput =e=> {
   if (a ??= [...t.children], r = $total, l = (e = e.target).selectedIndex) {
     if (--l < 3)
-      while (t.appendChild(a[o[l][r]]), --r);
+      while (t.appendChild(a[$orders[l][r]]), --r);
     else
       (location.href = "//ariamaranai.github.io")
   } else if (l == 0)
@@ -18,5 +18,5 @@ oninput =e=> {
 addEventListener("wheel", Number.isInteger(devicePixelRatio) ?
   e => e.preventDefault(scrollBy(0, e.deltaY > 0 ? 36 - t.scrollTop % 36 || 36 : - t.scrollTop % 36 || -36)) :
   e => e.preventDefault(scrollBy(0 ,e.deltaY > 0 ? 36 : -36)), { passive: !1 });
-let l, t = document.body, a, d, r, s=t.querySelector("s"), o=$orders
+let l, t = document.body, a, d, r, s=t.querySelector("s")
 }
