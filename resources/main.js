9e13481603d1a03871b32863879620bc30153257
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2024-12-10 10:58:45 +0900

    Add files via upload

--- resources/main.js
+++ resources/main.js
@@ -5,7 +5,7 @@ oninput =e=> {
     if (--l < 3)
       while (t.appendChild(a[$orders[l][r]]), --r);
     else
-      (location.href = "//ariamaranai.github.io")
+      location.href = "//ariamaranai.github.io"
   } else if (l == 0)
     while (t.prepend(a[--r]), r);
   else {
@@ -16,7 +16,7 @@ oninput =e=> {
   }
 },
 addEventListener("wheel", Number.isInteger(devicePixelRatio) ?
-  e => (e.preventDefault(),scrollBy(0, e.deltaY > 0 ? 36 - t.scrollTop % 36 || 36 : - t.scrollTop % 36 || -36)) :
-  e => (e.preventDefault(),scrollBy(0 ,e.deltaY > 0 ? 36 : -36)), { passive: !1 });
+  e => (e.preventDefault(), scrollBy(0, e.deltaY > 0 ? 36 - t.scrollTop % 36 || 36 : - t.scrollTop % 36 || -36)) :
+  e => (e.preventDefault(), scrollBy(0 ,e.deltaY > 0 ? 36 : -36)), { passive: !1 });
 let l, t = document.body, a, d, r, s=t.querySelector("s")
 }
