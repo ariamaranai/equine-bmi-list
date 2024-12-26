4f3343235fa045f646d239f0d51eadc820ef1cb5
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2024-12-26 09:31:54 +0900

    Add files via upload

--- resources/build.js
+++ resources/build.js
@@ -7,9 +7,9 @@ let hrefing = href =>
     : "//www.pedigreequery.com/" +
       href.slice(30).split("+").map(v => v[0].toUpperCase() + v.slice(1)).join("+");
 let toIndexOf = v => hrefs.indexOf(v.href) + 1;
-let wts = heights.toSorted((a, b) => b.wt - a.wt).map(toIndexOf);
-let bmis = heights.toSorted((a, b) => b.bmi - a.bmi).map(toIndexOf);
-let years = heights.toSorted((a, b) => a.year - b.year).map(toIndexOf);
+let wts = heights.toSorted((a, b) => b.wt - a.wt || -1).map(toIndexOf);
+let bmis = heights.toSorted((a, b) => b.bmi - a.bmi || -1).map(toIndexOf);
+let years = heights.toSorted((a, b) => a.year - b.year || -1).map(toIndexOf);
 let total = heights.length;
 let orders = `[[0,${wts}],[0,${bmis}],[0,${years}]]`;
 let css =
